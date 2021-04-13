6713b25ed0d1d3af244b8b428e2ad1a9125044e4
an.aslanian <an.aslanian@gmail.com>
2021-04-13 02:10:49 +0200

    adding gps

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivymd.app import MDApp
 from app_lib.mymapview import MyMapView
 from app_lib.searchpopupmenu import SearchPopupMenu
+from app_lib.gpshelper import GpsHelper
 import sqlite3
 
 class MainApp(MDApp):
@@ -13,6 +14,7 @@ class MainApp(MDApp):
     
     def on_start(self):
         """initialize GPS"""
+        GpsHelper().run()
 
         """connect to database"""
         self.connection = sqlite3.connect("db_utils/dogs_database.db")
